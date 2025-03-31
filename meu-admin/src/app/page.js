f344440fd089f4bdc06117ee72b86ec273b7d06b
Assane Omar <assaneomar@MacBook-Air-de-Assane.local>
2025-03-31 02:20:07 +0200

    feat add: meu-admin

---
+++ meu-admin/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <div>
+          <h1>Painel Administrativo</h1>
+          <a href="https://seu-usuario.github.io/meu-site/">Voltar ao site</a>
+      </div>
+  );
+}
+
